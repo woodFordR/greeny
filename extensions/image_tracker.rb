cf71d09a0201f6346e0147d1c49a51f88a412ad6
woodFordR <adam@kaewell.com>
2024-03-31 04:14:22 -0400

    added better documentation for rdoc, cleaned up extension methods, added styling

--- extensions/image_tracker.rb
+++ extensions/image_tracker.rb
@@ -7,20 +7,28 @@ class ImageTracker < Middleman::Extension
 
   def initialize(app, options_hash={}, &block)
     super
-
     require "open-uri"
   end
+
+  ##
+  # The grab_links method parses a website for image urls
 
   def grab_links
     image_urls = URI.open("https://drop.com/buy/drop-olkb-preonic-acute-keycaps") do |f|
       f.read.scan(/.*?<link.*?as="image" href=(".*?").*?/m)
     end
-    image_urls.map!{ |f| /.*(https.*\.png).*/.match(f[0])[1] }
+    image_urls.map { |f| /.*(https.*\.png).*/.match(f[0])[1] }
   end
 
   helpers do
-    def make_links(text)        
-      "<img src='#{grab_links[0]}'>'#{text}'</img>"
+
+    ##
+    # The make_links helper method allows a template to display the image url
+
+    def make_links()
+      link_tos = grab_links.map { |url| "<img src=#{url} />" }
+
+      link_tos.join("\n")
     end
   end
 end
